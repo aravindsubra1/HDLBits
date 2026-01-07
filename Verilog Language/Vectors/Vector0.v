9853243e142e16001f277374dd8faba16f65ef35
aravindsubra1 <c837839962@colostate.edu>
2026-01-07 12:02:53 -0700

    Delete Verilog Language/Vectors directory

--- Verilog Language/Vectors/Vector0.v
+++
@@ -1,9 +0,0 @@
-module top_module
-    (
-        input wire [2:0]vec,
-        output wire [2:0] outv,
-        output wire o0, o1, o2
-    );
-    assign outv = vec;
-    assign {o0, o1, o2} = {vec[0], vec[1], vec[2]};
-endmodule
--- Verilog Language/Vectors/Vector5.v
+++
@@ -1,9 +0,0 @@
-module top_module
-    (
-        input wire a,b,c,d,e,
-        output wire [24:0] out
-    );
-    
-    assign out = ~( {{5{a}},{5{b}},{5{c}},{5{d}},{5{e}}}^{5{a,b,c,d,e}} );
-    
-endmodule
